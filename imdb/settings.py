17e54ef31e74e99eaec099094f298db85a29cd7a
manan gandhi <gandhimanan1@gmail.com>
2024-11-10 14:17:54 -0500

    added 3 kindsoff validation types

--- imdb/settings.py
+++ imdb/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-qq02v(0intul2pb!9z$96lnq$r!fzleqq7#4tahbjf0m0z237c"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -2,10 +2,19 @@ from rest_framework import serializers
 from watchlist_app.models import Movie
 
 
+# Type 3: Validator
+def description_length(data):
+    if len(data) < 5 or len(data) > 200:
+        raise serializers.ValidationError(
+            "Description must be between 5 and 200 characters long."
+        )
+    return data
+
+
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField()
-    description = serializers.CharField()
+    description = serializers.CharField(validators=[description_length])
     active = serializers.BooleanField()
 
     def create(self, validated_data):
@@ -17,3 +26,21 @@ class MovieSerializer(serializers.Serializer):
         instance.active = validated_data.get("active", instance.active)
         instance.save()
         return instance
+
+    # Type 1: Field Leval Validator
+    def validate_name(self, value):
+        if len(value) < 2 or len(value) > 50:
+            raise serializers.ValidationError(
+                "Name must be between 5 and 50 characters long."
+            )
+        return value
+
+    # Type 2: Object Leval Validation
+    def validate(self, data):
+        if data["name"] == data["description"]:
+            raise serializers.ValidationError(
+                "Title and Description cannot be the same."
+            )
+        return data
+
+    # Type 3: Validator defined on the top
