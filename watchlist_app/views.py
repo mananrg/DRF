d2736622aa71aed8c0588d87551e0f513b5625db
manan gandhi <gandhimanan1@gmail.com>
2024-11-09 19:57:19 -0500

    1_API_With_Django

--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from .models import Movie
+from django.http import JsonResponse
+
 
 # Create your views here.
+def movie_list(request):
+    """
+    This function retrieves a list of movies from the database and renders them in a template.
+
+    Parameters:
+    request (HttpRequest): The incoming request object containing information about the client.
+
+    Returns:
+    HttpResponse: The rendered template with the list of movies.
+    """
+    movies = Movie.objects.all()
+    data = {"movies": list(movies.values())}
+    return JsonResponse(data)
+
+
+def movie_details(request, pk):
+    movie = Movie.objects.get(pk=pk)
+    print(movie)
+    data = {
+        "movie": movie.name,
+        "description": movie.description,
+        "active": movie.active,
+    }
+    return JsonResponse(data)
